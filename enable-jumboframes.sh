965c63a05b30e053fe9084b84acdcd1137cf5f72
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2024-11-27 12:10:18 -0800

    mtu script

---
+++ enable-jumboframes.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#raise MTU's on bonded interfaces to 9000 
+#note: these will automatically propagate to physical interfaces
+echo ~~~Raising interface MTUs to 9000 bytes~~~
+ip link set mtu 9000 dev bond0
+ip link set mtu 9000 dev bond0.2636
+
+ip a | grep mtu
