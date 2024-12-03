a07ad6ed939e475e60a3ad0ff2626d8ad5cae2b6
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2024-12-02 16:18:00 -0800

    pod interface config

--- launch-pod.sh
+++ launch-pod.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+echo ~~~Starting container~~~
+docker run -d --name pod  --cap-add=NET_ADMIN --net vxlan-bridge --ip 172.18.0.12 --gpus all nvidia/cuda:12.4.0-base-ubuntu22.04 sleep 1000
+docker exec pod apt update
+docker exec pod apt install iproute2
 
-docker run -d --net vxlan-net --ip 172.18.0.11 --gpus all nvidia/cuda:12.4.0-base-ubuntu22.04 sleep infinity
+echo ~~~Configuring interface~~~
+docker exec pod ip link set dev eth0 mtu 8950
+ip a | grep mtu
