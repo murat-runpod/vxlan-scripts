64cc103c2cddea7851a73a701db9b0b2a764ff56
Murat Magomedov <muratmagomedov@Murats-MacBook-Pro.local>
2024-12-03 14:16:48 -0800

    ensure Docker bridge MTU consistent with pods

--- create-pod.sh
+++ create-pod.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo ~~~Creating container~~~
-docker run -d --name pod  --cap-add=NET_ADMIN --net vxlan-bridge --ip 172.18.0.12 --gpus all nvidia/cuda:12.4.0-base-ubuntu22.04 sleep 10000
+docker run -d --name pod --net vxlan-bridge --ip 172.18.0.12 --gpus all nvidia/cuda:12.4.0-base-ubuntu22.04 sleep 10000
 docker exec pod apt update
 docker exec pod apt install iproute2 -y
 
 echo ~~~Configuring interface~~~
-docker exec pod ip link set dev eth0 mtu 8950
+#docker exec pod ip link set dev eth0 mtu 8950
 docker exec pod ip a | grep mtu
 
 echo ~~~Starting Pod~~~
--- start-pod.sh
+++ start-pod.sh
@@ -2,6 +2,6 @@
 
 echo ~~~Starting Pod~~~
 docker start pod
-docker exec pod ip link set dev eth0 mtu 8950
+#docker exec pod ip link set dev eth0 mtu 8950
 docker exec -it pod bash
 
